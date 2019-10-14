e147348bce25e6781cf252b48482b108edd058f5
andoreyrich <andoreyrich@gmail.com>
2019-10-14 15:54:46 +0200

    Pages with modul "Route"

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import Header from "./Header/Header";
 import NavBar from "./NavBar/NavBar";
 import Profile from "./Profile/Profile";
 import Dialogs from "./Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 const App = (props) => {
 	return (
-		<div className="app-wrapper">
-			<Header/>
-			<NavBar/>
-			<div className="app-wrapper-content">
-				<Profile/>
-				{/*<Dialogs/>*/}
+		<BrowserRouter>
+			<div className="app-wrapper">
+				<Header/>
+				<NavBar/>
+				<div className="app-wrapper-content">
+					<Route path="/profile" component={Profile}/>
+					<Route path="/dialogs" component={Dialogs}/>
+					<Route path="/news" component={Dialogs}/>
+					<Route path="/music" component={Dialogs}/>
+					<Route path="/settings" component={Dialogs}/>
+				</div>
 			</div>
-		</div>
+		</BrowserRouter>
 	);
 }
 
