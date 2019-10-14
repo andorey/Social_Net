bfca41cd9ce701eaaf70504597d7598442e5c1b3
andoreyrich <andoreyrich@gmail.com>
2019-10-14 14:28:00 +0200

    add Dialogs

---
+++ src/Dialogs/Dialog/Dialog.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Dialog.module.css';
+
+const Dialog = () => {
+	return (
+		<div className={s.dialog}>
+			Dialog
+		</div>
+	);
+}
+
+export default Dialog;
