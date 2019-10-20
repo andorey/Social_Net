0ac33f08c462f0edff02e8db62d3b13729e15e52
andoreyrich <andoreyrich@gmail.com>
2019-10-20 19:03:39 +0200

    redacting button and text

--- src/Profile/Posts/Post/Post.jsx
+++ src/Profile/Posts/Post/Post.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import s from './Post.module.css';
 
 const Post = (props) => {
+
 	return (
 		<div className={s.post}>
 			<div className={s.item}>
-				<img
-					src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa-Rtjo86Y2Wo-a_WCxI-Eao3f-L9VbK9eGMoBSg-Pq5dhkaSd"
-					alt=""/>
-				{props.message}
+				<div className={s.photo}>
+					<img
+						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa-Rtjo86Y2Wo-a_WCxI-Eao3f-L9VbK9eGMoBSg-Pq5dhkaSd"
+						alt=""/>
+					{props.message}
+				</div>
 				<div>
 					<span>like</span>
 					{props.likesCount}
