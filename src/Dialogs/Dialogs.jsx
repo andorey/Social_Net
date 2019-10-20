9ca29b1084f94d172f105ebfac2b6d8a9db9913a
andoreyrich <andoreyrich@gmail.com>
2019-10-20 20:55:25 +0200

    create sequences datas messages and dialogs

--- src/Dialogs/Dialogs.jsx
+++ src/Dialogs/Dialogs.jsx
@@ -4,17 +4,30 @@ import Message from "./Messages/Message";
 import Dialog from "./Dialog/Dialog";
 
 const Dialogs = (props) => {
+
+	let dialogsData =[
+		{id: 1, name: 'Andrew'},
+		{id: 2, name: 'Dimych'},
+		{id: 3, name: 'Alex'},
+	]
+
+	let messagesData =[
+		{id: 1, message: 'Hi, how are you?'},
+		{id: 2, message: "Are you O'k?"},
+		{id: 3, message: 'Good night my friends. I go to bed'},
+	]
+
 	return (
 		<div className={s.dialogs}>
 			<div className={s.dialogsItem}>
-				<Dialog name="Andrew" id="1"/>
-				<Dialog name="Dimych" id="2"/>
-				<Dialog name="Alex" id="3"/>
+				<Dialog name={dialogsData[0].name} id={dialogsData[0].id}/>
+				<Dialog name={dialogsData[1].name} id={dialogsData[1].id}/>
+				<Dialog name={dialogsData[2].name} id={dialogsData[2].id}/>
 			</div>
 			<div className={s.messages}>
-				<Message message="Hi, how are you?" />
-				<Message message="Are you O'k?" />
-				<Message message="Good night my friends!" />
+				<Message message={messagesData[0].message} />
+				<Message message={messagesData[1].message} />
+				<Message message={messagesData[2].message} />
 			</div>
 		</div>
 	);
