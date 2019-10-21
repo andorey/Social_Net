74f84f954d56ef96ebcde6626fbd0e8a30472f72
andoreyrich <andoreyrich@gmail.com>
2019-10-21 20:27:21 +0200

    Updates

--- precache-manifest.1a76f865aaa4bee813210e47a8e32457.js
+++ precache-manifest.1a76f865aaa4bee813210e47a8e32457.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "03680e34ced21d1bf1d5e1871a01af80",
+    "revision": "ff5578b5b4c8ae62038f5fce120ab4e1",
     "url": "/social_net/index.html"
   },
   {
-    "revision": "944c9e1eb84c32931d9f",
-    "url": "/social_net/static/css/main.b13095e8.chunk.css"
+    "revision": "6a64765771b6f217a373",
+    "url": "/social_net/static/css/main.24b724d4.chunk.css"
   },
   {
     "revision": "415099887b03e4a56cd4",
     "url": "/social_net/static/js/2.a282515f.chunk.js"
   },
   {
-    "revision": "944c9e1eb84c32931d9f",
-    "url": "/social_net/static/js/main.7ea6837c.chunk.js"
+    "revision": "6a64765771b6f217a373",
+    "url": "/social_net/static/js/main.b0ed3349.chunk.js"
   },
   {
     "revision": "620025d80de470800529",
