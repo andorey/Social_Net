6705d49024ea637646b39892237bedad79fdf107
andoreyrich <andoreyrich@gmail.com>
2019-11-01 17:13:51 +0100

    replace teg <BrowserRouter> in index.js && changed settings CSS Profile.jsx

--- src/App.js
+++ src/App.js
@@ -4,33 +4,32 @@ import Header from "./Header/Header";
 import NavBar from "./NavBar/NavBar";
 import Profile from "./Profile/Profile";
 import Dialogs from "./Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 
 
 const App = (props) => {
 
 	return (
-		<BrowserRouter>
-			<div className="app-wrapper">
-				<Header/>
-				<NavBar/>
-				<div className="app-wrapper-content">
-					{/*<Route path="/profile" component={Profile}/>*/}
-					{/*<Route path="/dialogs" component={Dialogs}/>*/}
-					{/*<Route path="/news" component={Dialogs}/>*/}
-					{/*<Route path="/music" component={Profile}/>*/}
-					{/*<Route path="/settings" component={Profile}/>*/}
+		<div className="app-wrapper">
+			<Header/>
+			<NavBar/>
+			<div className="app-wrapper-content">
+				{/*<Route path="/profile" component={Profile}/>*/}
+				{/*<Route path="/dialogs" component={Dialogs}/>*/}
+				{/*<Route path="/news" component={Dialogs}/>*/}
+				{/*<Route path="/music" component={Profile}/>*/}
+				{/*<Route path="/settings" component={Profile}/>*/}
 
-					<Route path="/profile"
-						   render={() => <Profile
-							   state={props.state.postPage} />}/>
-					<Route path="/dialogs"
-						   render={() => <Dialogs
-							   state={props.state.dialogsPage}/>}/>
+				<Route path="/profile"
+					   render={() => <Profile
+						   state={props.state.postPage}/>}/>
+				<Route path="/dialogs"
+					   render={() => <Dialogs
+						   state={props.state.dialogsPage}/>}/>
 
-				</div>
+
 			</div>
-		</BrowserRouter>
+		</div>
 	);
 }
 
