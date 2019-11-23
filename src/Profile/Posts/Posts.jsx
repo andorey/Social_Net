e46013cdf572f908c8d6b06461127d75f399b9bd
andoreyrich <andoreyrich@gmail.com>
2019-11-23 21:11:29 +0100

    added in Redux action creator , action type

--- src/Profile/Posts/Posts.jsx
+++ src/Profile/Posts/Posts.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import s from './Posts.module.css';
 import Post from "./Post/Post";
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../redux/state";
+
 
 
 const Posts = (props) => {
@@ -10,20 +12,20 @@ const Posts = (props) => {
 	let newPostElement = React.createRef();
 
 	let addPost = () => {
-		props.dispatch({type: 'ADD-POST'});
+		props.dispatch(addPostActionCreator());
 	};
 
 	let onPostChange = () => {
 		let text = newPostElement.current.value;
 
-		let action = {type: 'UPDATE-NEW-POST-TEXT', newText: text};
+		let action = updateNewPostTextActionCreator(text);
 		props.dispatch( action );
 	};
 
 
 	let oneAddTaskKeyPress = (e) => {
 		if(e.key === 'Enter'){
-			props.addPost();
+			addPost();
 		}
 	};
 
